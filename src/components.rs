1c1e399c5e055ee2f698e9acf60d4aba7cff0552
Yusei Honzawa <->
2025-01-10 18:45:59 +0900

    [refactor]main関数とheader_draw関数を分離させました

---
+++ src/components.rs
@@ -0,0 +1,25 @@
+use crossterm::{cursor, style, terminal, execute};
+
+use std::io;
+use std::error;
+
+pub fn header_draw(text: &str) -> Result<(), Box<dyn error::Error>>{
+    // op = original position
+    let ( op_x, op_y ) = cursor::position()?;
+    let ( size_x, _ ) = terminal::size()?;
+
+    execute!(io::stdout(), cursor::MoveTo(0,0))?;
+    execute!(io::stdout(), style::SetBackgroundColor(style::Color::Red))?;
+    execute!(io::stdout(), style::SetForegroundColor(style::Color::White))?;
+
+    let repeat_count = ( size_x as usize - text.len() ) - 1;
+
+    print!(" {}{}", text, " ".repeat(repeat_count));
+
+    execute!(io::stdout(), style::SetBackgroundColor(style::Color::Reset))?;
+    execute!(io::stdout(), style::SetForegroundColor(style::Color::Reset))?;
+
+    execute!(io::stdout(), cursor::MoveTo(op_x, op_y))?;
+
+    Ok(())
+}
